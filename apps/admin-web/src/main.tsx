f0183bd28abf63ceba39d30837378ec707a38dcb
Gonzalo <gnpantoja@outlook.com>
2022-04-13 22:48:37 -0500

    Add MUI

--- apps/admin-web/src/main.tsx
+++ apps/admin-web/src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import App from './app/app';
 
