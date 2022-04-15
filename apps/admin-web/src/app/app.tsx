805538c4bc796c30b5b078655744a12eaf346923
Gonzalo Pantoja <16733681+Gonzalo8642@users.noreply.github.com>
2022-04-14 20:18:17 -0500

    Gonzalo8642/admin UI (#4)
    Co-authored-by: Gonzalo <gnpantoja@outlook.com>

--- apps/admin-web/src/app/app.tsx
+++ apps/admin-web/src/app/app.tsx
@@ -1,28 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import { Message } from '@caribou-crew/mezzo-api-interfaces';
+import { UserRoute } from '@caribou-crew/mezzo-api-interfaces';
+
+import {
+  TextField,
+  Stack,
+  Paper,
+  Autocomplete,
+  Grid,
+  Box,
+  Container,
+} from '@mui/material';
+import RouteItem from './components/RouteItem';
+import Header from './components/Header';
 
 export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
+  const [routes, setRoutes] = useState<UserRoute[]>([]);
 
   useEffect(() => {
-    fetch('/api')
-    // fetch('/respondWithFile')
+    fetch('/mezzo/routes')
       .then((r) => r.json())
-      .then(setMessage);
+      .then((r) => setRoutes(r.routes));
   }, []);
 
+  const list = () => {
+    const listOfRoutes: any[] = [];
+    if (Array.isArray(routes) && routes.length > 0) {
+      routes.forEach((route) => {
+        listOfRoutes.push(<RouteItem route={route} key={route.id}></RouteItem>);
+      });
+    }
+    return listOfRoutes;
+  };
   return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to admin-web!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
-      </div>
-      <div>{m.message}</div>
-    </>
+    <Container component="main" maxWidth="lg">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12} justifyContent="center">
+            <Header name="Mezzo"></Header>
+            <Autocomplete
+              disablePortal
+              freeSolo
+              id="combo-box-search"
+              options={routes.map((route) => ({
+                label: route.path,
+                id: route.id,
+              }))}
+              sx={{ pr: '20%', pl: '20%' }}
+              renderInput={(params) => (
+                <TextField {...params} label="Search..." />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Stack spacing={2}>{list()}</Stack>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
---
+++ apps/admin-web/src/app/components/Header.tsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+
+type Props = {
+  name: string;
+};
+
+export default function Headers(props: Props) {
+  const navItems = [
+    { label: 'Reset State', path: '/state/reset' },
+    { label: 'Reset Route Settings', path: '/state/reset' },
+  ];
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = (navItem?: { label: string; path: string }) => {
+    if (navItem) {
+      fetch(navItem.path);
+    }
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ mb: 5 }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            {props.name}
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={() => handleCloseNavMenu()}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {navItems.map((navItem) => (
+                <MenuItem
+                  key={navItem.label}
+                  onClick={() => handleCloseNavMenu(navItem)}
+                >
+                  <Typography textAlign="center">{navItem.label}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {navItems.map((navItem) => (
+              <Button
+                key={navItem.label}
+                onClick={() => handleCloseNavMenu(navItem)}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {navItem.label}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
