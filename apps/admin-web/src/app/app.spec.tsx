e50a778a300d1ad45dd52ebf52b7fe6fd596a757
Gonzalo <gnpantoja@outlook.com>
2022-04-24 10:47:43 -0500

    Update app.spec.tsx

--- apps/admin-web/src/app/app.spec.tsx
+++ apps/admin-web/src/app/app.spec.tsx
@@ -22,6 +22,6 @@ describe('App', () => {
     });
 
     const { baseElement } = render(<App />);
-    await waitFor(() => getAllByText(baseElement, 'Filter'));
+    await waitFor(() => getAllByText(baseElement, 'Search'));
   });
 });
