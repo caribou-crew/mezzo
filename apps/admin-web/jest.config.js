3c945e2e67c3c38390b5ec17635fbce0159aef1b
Gonzalo <gnpantoja@outlook.com>
2022-04-24 10:46:44 -0500

    jest to fall back to lodash

--- apps/admin-web/jest.config.js
+++ apps/admin-web/jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
+  moduleNameMapper: {
+    '^lodash-es$': 'lodash',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/admin-web',
 };
