b4d6e227201b74ec078f6183bbebcefba4b8e5ec
Steven Goff <steveng9@gmail.com>
2022-04-27 18:47:04 -0500

    Only show version when API has valid response

--- apps/admin-web/src/app/app.tsx
+++ apps/admin-web/src/app/app.tsx
@@ -195,7 +195,7 @@ export const App = () => {
               _renderTypography()
             )}
             <Typography align="center" sx={{ mt: 5 }}>
-              v{version}
+              {version ? `v${version}` : null}
             </Typography>
           </Grid>
         </Grid>
