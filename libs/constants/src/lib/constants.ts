2c9d0e82021dfc90245cf629ed93cfcf423fc56c
Steven Goff <steveng9@gmail.com>
2022-05-06 23:30:30 -0500

    Update constants and interfaces

--- libs/constants/src/lib/constants.ts
+++ libs/constants/src/lib/constants.ts
@@ -1,8 +1,13 @@
 export const X_REQUEST_VARIANT = 'x-request-variant';
 export const X_REQUEST_SESSION = 'x-request-session';
 export const DEFAULT_VARIANT = 'default';
-export const MEZZO_API_PATH = '/_admin/api';
 export const LOCAL_HOST = 'localhost';
 export const DEFAULT_PORT = 8000;
 export const DEFAULT_VARIANT_CATEGORY = 'Variants';
 export const GLOBAL_VARIANT_CATEGORY = 'Global Variants';
+
+// API endpoints
+export const MEZZO_API_PATH = '/_admin/api';
+export const MEZZO_API_POST_RECORD_REQUEST = `${MEZZO_API_PATH}/record/request`;
+export const MEZZO_API_POST_RECORD_RESPONSE = `${MEZZO_API_PATH}/record/response`;
+export const MEZZO_API_GET_RECORDINGS = `${MEZZO_API_PATH}/recordings`;
