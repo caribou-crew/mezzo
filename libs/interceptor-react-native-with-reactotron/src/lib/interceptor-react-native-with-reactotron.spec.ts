e9db4b3956500d9e4c318f8d57b10ca432c18b50
Steven Goff <steveng9@gmail.com>
2022-05-11 13:52:14 -0500

    Remove auto generated spec file

--- libs/interceptor-react-native-with-reactotron/src/lib/interceptor-react-native-with-reactotron.spec.ts
+++
@@ -1,9 +0,0 @@
-import { interceptorReactNativeWithReactotron } from './interceptor-react-native-with-reactotron';
-
-describe('interceptorReactNativeWithReactotron', () => {
-  it('should work', () => {
-    expect(interceptorReactNativeWithReactotron()).toEqual(
-      'interceptor-react-native-with-reactotron'
-    );
-  });
-});
