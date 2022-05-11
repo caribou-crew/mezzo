5c9897d8e11e7fae44f73c3292b28543bf8ee9e8
Steven Goff <steveng9@gmail.com>
2022-05-10 23:43:35 -0500

    Support scss

--- apps/admin-web/src/main.tsx
+++ apps/admin-web/src/main.tsx
@@ -4,6 +4,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import 'react-reflex/styles.css';
+import './styles.scss';
 
 import App from './app/app';
 
