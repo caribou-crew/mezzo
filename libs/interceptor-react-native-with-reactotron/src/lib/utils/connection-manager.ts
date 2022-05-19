5122e1bb45135de44f30276673b449fa2771ab52
Steven Goff <steveng9@gmail.com>
2022-05-19 15:56:47 -0500

    fix(react-native): fix socket connection in react-native so that it can send

--- libs/interceptor-react-native-with-reactotron/src/lib/utils/connection-manager.ts
+++ libs/interceptor-react-native-with-reactotron/src/lib/utils/connection-manager.ts
@@ -1,8 +1,17 @@
 export default function connectionManager(path: string) {
   const ws = new WebSocket(path);
   return {
-    send: ws.send,
-    close: ws.close,
+    // send: ws.send,
+    // close: ws.close,
+    send: function send(payload) {
+      ws.send(payload);
+    },
+    close: function close() {
+      console.log(
+        '[interceptor-react-native.connectionManager close] RN socket close'
+      );
+      ws.close();
+    },
     set onopen(cb: () => void) {
       ws.onopen = cb;
     },
