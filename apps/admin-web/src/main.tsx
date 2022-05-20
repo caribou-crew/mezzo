c987cf542dce562cc6dbc11914e532937d1bea2b
NSwanson1 <46030941+NSwanson1@users.noreply.github.com>
2022-05-19 22:28:37 -0500

    feat(client): add Theming and Styling to Pages
    fix #109

--- apps/admin-web/src/main.tsx
+++ apps/admin-web/src/main.tsx
@@ -1,5 +1,6 @@
 // import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,12 +10,22 @@ import './styles.scss';
 
 import App from './app/app';
 
+const appTheme = createTheme({
+  palette: {
+    background: {
+      default: '#F8F8F9',
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   // Strict mode renders everything 2x in dev mode which can be confusing when troubleshooting why useEffect is called 2x to fire API calls
   // <StrictMode>
-  <App />
+  <ThemeProvider theme={appTheme}>
+    <App />
+  </ThemeProvider>
   // </StrictMode>
 );
