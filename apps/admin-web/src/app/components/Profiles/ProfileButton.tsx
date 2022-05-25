ce946b0776a98ef7b0c742a1a78e69db8785d453
Steven Goff <steveng9@gmail.com>
2022-05-24 23:06:56 -0500

    fix: update profile on select (#141)

--- apps/admin-web/src/app/components/Profiles/ProfileButton.tsx
+++ apps/admin-web/src/app/components/Profiles/ProfileButton.tsx
@@ -1,4 +1,4 @@
-import { Profile } from '@caribou-crew/mezzo-interfaces';
+import { Profile, RouteVariant } from '@caribou-crew/mezzo-interfaces';
 import mezzoClient from '@caribou-crew/mezzo-core-client';
 import { IconButton } from '@mui/material';
 import { CheckBox, CheckBoxOutlineBlank } from '@mui/icons-material';
@@ -9,18 +9,21 @@ const ProfileButton = ({
   client,
   profile,
   selectedProfile,
+  setActiveVariants,
 }: {
   profile: Profile;
   client: ReturnType<typeof mezzoClient>;
   setMatchedProfile: (arg0: string) => void;
   setSelectedProfile: (arg0: string) => void;
   selectedProfile: string;
+  setActiveVariants: (arg0: RouteVariant[]) => void;
 }) => {
   return (
     <IconButton
       color="primary"
       onClick={() => {
         client.setMockVariant(profile.variants);
+        setActiveVariants(profile.variants);
         setSelectedProfile(profile.name);
         setMatchedProfile(profile.name);
       }}
