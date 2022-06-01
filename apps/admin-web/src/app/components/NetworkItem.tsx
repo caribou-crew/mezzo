c80f70b26229cb0a727b9265d3316cd46563d884
NSwanson1 <46030941+NSwanson1@users.noreply.github.com>
2022-05-31 22:56:23 -0500

    feat: update Styling on Recording Screen

--- apps/admin-web/src/app/components/NetworkItem.tsx
+++ apps/admin-web/src/app/components/NetworkItem.tsx
@@ -1,6 +1,8 @@
 import { TableCell, TableRow } from '@mui/material';
 
 import { RecordedItem } from '@caribou-crew/mezzo-interfaces';
+import { Cancel, Warning, CheckBox, ChangeCircle } from '@mui/icons-material';
+import useWindowDimensions from '../hooks/useWindowDimensions';
 
 interface Props extends RecordedItem {
   onClick: any;
@@ -16,20 +18,72 @@ const NetworkItem = ({
   duration,
   onClick,
 }: Props) => {
+  const { width } = useWindowDimensions();
+  const host = new URL(url ?? '').origin;
+  const path = new URL(url ?? '').pathname;
+  const time = new Date(date).getTime();
+
+  const getStatusSymbol = (status: any) => {
+    let icon = <ChangeCircle />;
+
+    switch (status) {
+      case 200:
+      case 201:
+        icon = <CheckBox color="success" />;
+        break;
+
+      case 500:
+      case 504:
+        icon = <Cancel color="error" />;
+        break;
+      case 400:
+      case 404:
+      case 411:
+      case 417:
+        icon = <Warning color="warning" />;
+        break;
+
+      default:
+        icon = <ChangeCircle color="primary" />;
+        break;
+    }
+
+    return icon;
+  };
   return (
     <TableRow
       key={uuid}
       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
       onClick={onClick}
     >
-      <TableCell component="th" scope="row">
+      <TableCell padding="checkbox" component="th" scope="row">
+        {getStatusSymbol(response?.status)}
+      </TableCell>
+      <TableCell padding="checkbox" component="th" scope="row">
         {response?.status}
       </TableCell>
-      <TableCell align="left">{request.method}</TableCell>
-      <TableCell align="left">{url}</TableCell>
-      <TableCell align="right">{new Date(date).toISOString()}</TableCell>
-      <TableCell align="right">{deltaTime}</TableCell>
-      <TableCell align="right">{duration.toFixed(2)}</TableCell>
+      <TableCell padding="checkbox" align="left">
+        {request.method}
+      </TableCell>
+      <TableCell padding="normal" align="left">
+        {host}
+      </TableCell>
+      <TableCell padding="normal" align="left">
+        {path}
+      </TableCell>
+      {width >= 1024 && (
+        <>
+          <TableCell padding="normal" align="right">
+            {time} ms
+          </TableCell>
+          <TableCell padding="normal" align="right">
+            {deltaTime} ms
+          </TableCell>
+          <TableCell padding="checkbox" align="right">
+            {duration.toFixed(2)}
+          </TableCell>
+        </>
+      )}
     </TableRow>
   );
 };
